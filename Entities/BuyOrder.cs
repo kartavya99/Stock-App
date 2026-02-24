aae155f153a5435922a252f543ff6889e64bdbe8
kartavya <kartavyas@gmail.com>
2026-02-24 18:25:49 +1100

    Entity models added and DTO model class added for buy and sell request

---
+++ Entities/BuyOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    /// <summary>
+    /// Represents a buy order to purchase the stocks
+    /// </summary>
+    public class BuyOrder
+    {
+        [Key]
+        public Guid BuyOrderId { get; set; }
+
+        public string StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "Stock Name can't be null or empty")]
+        public string StockName { get; set; }
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 10000, ErrorMessage ="You can buy maximum of 10000 share in single order. Mimimum is 1.")]
+        public uint Quantity { get; set; }
+
+        [Range(1, 10000, ErrorMessage ="THe maximum price of stock is 10000. Mimimun is 1.")]
+        public double Price {  get; set; }                              
+
+    }
+}
---
+++ Entities/SellOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    /// <summary>
+    /// Represents a sell order to sell the stocks
+    /// </summary>
+    public class SellOrder
+    {
+        [Key]
+        public Guid SellOrderId { get; set; }
+
+        public string StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "Stock Name can't be null or empty")]
+        public string StockName { get; set; }
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "You can buy maximum of 10000 share in single order. Mimimum is 1.")]
+        public uint Quantity { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "THe maximum price of stock is 10000. Mimimun is 1.")]
+        public double Price { get; set; }
+    }
+}
